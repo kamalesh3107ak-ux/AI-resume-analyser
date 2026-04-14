dc7c096e7652479e40aeacfa81fd9f461a193e10
kamalesh3107ak-ux <kamalesh3107ak@gmail.com>
2026-04-14 12:18:18 +0530

    add files

--- app/routes/components/ResumeCard.tsx
+++ app/routes/components/ResumeCard.tsx
@@ -1,6 +1,20 @@
-const ResumeCard = () => {
+import { Link } from 'react-router';
+import ScoreCircle from '~/routes/components/ScoreCircle';
+
+const ResumeCard = ({resume:{id,companyName,jobTitle,feedback}}:{resume:Resume}) => {
   return (
-    <div>ResumeCard</div>
+    <Link to={`/resume/${resume.id}`} className="resume-card animate-in fade-in duration-1000 ">
+      <div className="flex flex-col gap-2 ">
+        <h2 className="text-black font-bold break-word ">{companyName}</h2>
+        <h3 className="text-lg break-word text-gray-500 ">{jobTitle}</h3>
+
+      </div>
+      <div className="flex-shrink-0 ">
+        <ScoreCircle score={feedback.overallScore}/>
+
+      </div>
+
+    </Link>
   )
 }
 export default ResumeCard
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from '~/routes/components/Navbar';
 import { resumes } from '~/constants';
+import ResumeCard from '~/routes/components/ResumeCard';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,22 +11,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className= "bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar/>
-<section className="main-section">
-  <div className="page-heading">
-    <h1> Track your Applications & Resume Ratings </h1>
-    <h2> Review your submissions and check AI-powered feedback. </h2>
-
-  </div>
-
-</section>
-    <div className="Resume-section">
-
-    </div>
-    {resumes.map(resume => (
-      <ResumeCard key = {resume.id} resume = {resume} />
-
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1> Track your Applications & Resume Ratings </h1>
+          <h2> Review your submissions and check AI-powered feedback. </h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="Resume-section">
+      {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
       ))}
-  </main>
+        </div>
+        )}
+    </main>
+  );
 }
