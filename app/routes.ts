dec4b933281c3d5e5a5ba9dd0d0bb758af5b0d4f
kamalesh3107ak-ux <kamalesh3107ak@gmail.com>
2026-04-19 13:58:24 +0530

    implement wipe funtionality

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,5 @@ export default [
   route('/auth', 'routes/auth.tsx'),
   route('/upload', 'routes/upload.tsx'),
   route('/resume/:id', 'routes/Resume.tsx'),
+  route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
